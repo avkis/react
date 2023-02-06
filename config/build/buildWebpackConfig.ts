b09c3fd0f028eb81f5411cb7833e45624b8b5769
Alexander Kiselev <avkiselev@mail.ru>
2023-02-06 20:43:43 +0300

    Added config files.

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,26 @@
+import webpack from 'webpack';
+import { buildDevServer } from './buildDevServer';
+import { buildLoaders } from './buildLoaders';
+import { buildPlugins } from './buildPlugin';
+import { buildResolvers } from './buildResolvers';
+
+import { BuildOptions } from './types/config';
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+	const {paths, mode, isDev} = options
+	return {
+		mode,
+		entry: paths.entry,
+		module: {
+			rules: buildLoaders(options),
+		},
+		resolve: buildResolvers(),
+		output: {
+			filename: '[name].[contenthash:8].js',
+			path: paths.build,
+			clean: true,
+		},
+		plugins: buildPlugins(options),
+		devtool: isDev ? 'inline-source-map' : undefined,
+		devServer: isDev ? buildDevServer(options) : undefined,
+	};
+}
